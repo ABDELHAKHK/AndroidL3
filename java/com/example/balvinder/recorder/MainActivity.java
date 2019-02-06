b2483d825dafe6b69b0180ce52cf813751fb1063
Joe <abdelhak-22@outlook.com>
2019-02-07 00:15:19 +0100

    first commit

---
+++ java/com/example/balvinder/recorder/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.balvinder.recorder;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button play,record;
+
+    //activité principale -UI
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) relativeLayout.getBackground();
+
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+
+        animationDrawable.start();
+
+        play=(Button) findViewById(R.id.play);
+        record=(Button) findViewById(R.id.record);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent= new Intent(MainActivity.this,PlayActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent intent= new Intent(MainActivity.this,RecordActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+    }
+}
